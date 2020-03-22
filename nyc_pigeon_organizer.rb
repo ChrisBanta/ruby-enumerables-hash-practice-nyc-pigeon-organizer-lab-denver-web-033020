062c1da61668c112adc3c357b195c819efb72173
Chris Banta <chrisbanta1@gmail.com>
2020-03-22 17:26:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,20 @@
 def nyc_pigeon_organizer(data)
-  new_pigeon_list = {
+  new_pigeon_list = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
 
-}
-}
+      if new_pigeon_list[name]
+      new_pigeon_list[name] = {}
+      end
+
+      if !new_pigeon_list[name][key]
+      new_pigeon_list[name][key] = []
+      end
+
+      new_pigeon_list[name][key] <<new_value.to_s
+      end
+    end
+  end
+   new_pigeon_list
 end
