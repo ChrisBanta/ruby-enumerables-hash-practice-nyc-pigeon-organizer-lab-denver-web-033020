0373e6da5772645c2dce1b9bf0d1d57f3db02a06
Chris Banta <chrisbanta1@gmail.com>
2020-03-22 17:35:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
     value.each do |new_value, names|    # create new_value and retriev names from data
       names.each do |name|              # retrieve name from names
 
-      if !new_pigeon_list[name]
+      if !new_pigeon_list[name]         # if name doesn't exist in new hash, create name with new hash
       new_pigeon_list[name] = {}
       end
 
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
       new_pigeon_list[name][key] = []
       end
 
-      new_pigeon_list[name][key] <<new_value.to_s
+      new_pigeon_list[name][key] <<new_value.to_s #convert to string
       end
     end
   end
