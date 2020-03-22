9576b539ee05a55e4552ebda63cf7d2b6d03623a
Chris Banta <chrisbanta1@gmail.com>
2020-03-22 17:53:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_list = {}                  # new empty hash
+  data.each do |key, value|             # retrieve each key and value from data
+    value.each do |new_value, names|    # create new_value and retriev names from data
+      names.each do |name|              # retrieve name from names
+
+      if !new_pigeon_list[name]         # if name doesn't exist in new hash, create name with new hash
+      new_pigeon_list[name] = {}
+      end
+
+      if !new_pigeon_list[name][key]
+      new_pigeon_list[name][key] = []
+      end
+
+      new_pigeon_list[name][key] <<new_value.to_s #convert to string
+      end
+    end
+  end
+   new_pigeon_list
 end
